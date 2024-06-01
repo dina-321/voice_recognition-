5607df4f5258f08d504a030ab8abda0f25353ab7
dina-321 <162456097+dina-321@users.noreply.github.com>
2024-06-02 01:26:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ def upload_file():
             os.remove(file_path)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
