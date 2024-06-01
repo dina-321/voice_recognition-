0a8d7f8c5c76032a216fe76d3634f142d7c498e0
dina-321 <162456097+dina-321@users.noreply.github.com>
2024-06-02 01:26:12 +0300

    Update patch_datasets.py

--- patch_datasets.py
+++ patch_datasets.py
@@ -1,5 +1,10 @@
 import datasets.builder
 from datasets.utils.version import Version
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, fields
 
-datasets.builder.BuilderConfig.__dataclass_fields__['version'].default_factory = lambda: Version("1.0.0")
+# Patch the BuilderConfig to set default_factory for version field
+config_fields = fields(datasets.builder.BuilderConfig)
+for field in config_fields:
+    if field.name == 'version':
+        field.default_factory = lambda: Version("1.0.0")
+        break
